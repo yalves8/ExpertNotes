0c8a88bf367a0aebe8304b8bcd971ad5ef6b216a
yalves8 <yasminleticia_@hotmail.com>
2024-02-09 01:51:11 -0300

    Custom Modal and states

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
 const note = {
-  date : new Date(),
-  content: "Hello Word"
-}
+  date: new Date(),
+  content: "Hello Word",
+};
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -20,20 +21,14 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3 overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar Nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida em texto
-            automaticamente.
-          </p>{" "}
-        </div>
+        <NewNoteCard />
         <NoteCard note={note} />
-        <NoteCard  note={{
-          date:new Date(2024,1,1),
-          content:"Primeira Nota de Testes",
-        }}/>
+        <NoteCard
+          note={{
+            date: new Date(2024, 1, 1),
+            content: "Primeira Nota de Testes",
+          }}
+        />
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {App} from './app.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./app.tsx";
+import { Toaster } from "sonner";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Toaster richColors />
+  </React.StrictMode>
+);
